b0626db97fdd02ddfb1190b3bb0656a4dd76ccc5
Izaac Ramirez <izaacramirez@Izaacs-Laptop.local>
2024-11-12 18:16:37 -0800

    added apollo provider to app.tsx.

--- Develop/client/src/App.tsx
+++ Develop/client/src/App.tsx
@@ -1,14 +1,15 @@
+
 import './App.css';
 import { Outlet } from 'react-router-dom';
-
+import { ApolloProvider, client } from '@apollo/client'; // Import ApolloProvider
 import Navbar from './components/Navbar';
 
 function App() {
   return (
-    <>
+    <ApolloProvider client={client}>
       <Navbar />
       <Outlet />
-    </>
+    </ApolloProvider>
   );
 }
 
